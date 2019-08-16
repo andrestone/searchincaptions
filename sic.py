8225d10f361646c354c1e7b69b7e23488f684bd3
andrestone <andrestone@users.noreply.github.com>
2019-08-16 11:28:42 +0200

    Initial Commit

---
+++ sic.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from time import sleep
+import html
+import numpy
+import requests
+from youtube_transcript_api import YouTubeTranscriptApi
+
+TOKEN_PATH = [d for d in os.listdir() if "TOKEN_" in d]
+if len(TOKEN_PATH) == 0:
+    print("You have to create a directory with the name TOKEN_{your token}.\n"
+          "Example: TOKEN_IJHEO2J3KjkOEWIJKEKklj323\n")
+    sys.exit()
+
+key = TOKEN_PATH[0].replace("TOKEN_", "")
+
+if len(sys.argv) <= 3:
+    print("Search youtube videos based on content (captions)")
+    print('Usage: python sic.py <"search scope"> <maxresults> <"multiple strings" "like" "this">')
+    sys.exit()
+
+q = sys.argv[1]
+mr = sys.argv[2]
+words = []
+for x in range(3, len(sys.argv)):
+    words.append(sys.argv[x])
+
+response = requests.get(
+    'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults={mr}&order=viewCount&q='
+    '{q}&type=video&videoCaption=closedCaption&key={key}'.format(q=q, mr=mr, key=key))
+json = response.json()
+print('Found ' + str(len(json['items'])) + ' videos.')
+
+for i in json['items']:
+    found = False
+    sleep(1)
+    vid = i['id']['videoId']
+    vid_string = html.unescape(i['snippet']['title'])
+    ch_string = html.unescape(i['snippet']['channelTitle'])
+    try:
+        current_caption = YouTubeTranscriptApi.get_transcript(vid)
+    except YouTubeTranscriptApi.CouldNotRetrieveTranscript:
+        print('No captions for the video: {vid_string} ({vid})'.format(vid_string=vid_string, vid=vid))
+        continue;
+    for cc in current_caption:
+        for word in words:
+            if word.lower() in cc['text'].lower():
+                found = True
+                print(
+                    'Found occurrence for "{word}" at {vid_string} ({vid}) -> '.format(vid_string=vid_string, word=word,
+                                                                                       vid=vid) +
+                    'https://youtu.be/{vid}?t={time}'.format(vid_string=vid_string, word=word,
+                                                             vid=vid,
+                                                             time=int(numpy.ceil(cc['start'])))
+                )
+    if not found:
+        print('No occurrences found on video {vid_string} by {ch_string} ({i}/{tvid})'.format(ch_string=ch_string,
+                                                                                       i=json['items'].index(i) + 1,
+                                                                                       tvid=len(json['items']),
+                                                                                       vid_string=vid_string))
